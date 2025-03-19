525369ea0c35fb274486c8de37817f4493c50983
JoaquinRamirez98 <joaquin.ramirez170998@gmail.com>
2025-03-19 01:02:17 -0300

    probando linea app.run comentado

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ if __name__ == "__main__":
     else:
         print(f"Error al cargar el PDF")
     port = int(os.environ.get('PORT', 8000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    #app.run(debug=True, host="0.0.0.0", port=port)
